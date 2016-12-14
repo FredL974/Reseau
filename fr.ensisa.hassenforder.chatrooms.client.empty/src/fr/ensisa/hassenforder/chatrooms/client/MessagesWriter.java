bacd7edd762767c6d1daa749189bd46bc981f980
FredL974 <frederic.lusinier@gmail.com>
2016-12-14 08:13:43 +0100

    first

---
+++ fr.ensisa.hassenforder.chatrooms.client.empty/src/fr/ensisa/hassenforder/chatrooms/client/MessagesWriter.java
@@ -0,0 +1,14 @@
+package fr.ensisa.hassenforder.chatrooms.client;
+
+import java.io.OutputStream;
+
+import fr.ensisa.hassenforder.network.BasicAbstractWriter;
+
+public class MessagesWriter extends BasicAbstractWriter {
+
+	public MessagesWriter(OutputStream outputStream) {
+		super (outputStream);
+		System.out.println("test");
+	}
+
+}
